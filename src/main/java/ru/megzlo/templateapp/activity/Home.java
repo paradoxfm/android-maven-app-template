9747e32923b77d3606082abad96cd25f99f85333
paradoxfm <paradoxfm@mail.ru>
2015-09-29 12:27:51 +0500

    project init

---
+++ src/main/java/ru/megzlo/templateapp/activity/Home.java
@@ -0,0 +1,21 @@
+package ru.megzlo.templateapp.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author paradoxfm - 28.09.2015
+ */
+
+public class Home extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
