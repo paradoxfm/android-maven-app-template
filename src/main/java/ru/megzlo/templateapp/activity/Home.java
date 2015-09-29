7a5ca361758b5d1b464fd3de64d39f4b43d08c76
paradoxfm <paradoxfm@mail.ru>
2015-09-29 18:02:48 +0500

    fixes

--- src/main/java/ru/megzlo/templateapp/activity/Home.java
+++ src/main/java/ru/megzlo/templateapp/activity/Home.java
@@ -2,11 +2,13 @@ package ru.megzlo.templateapp.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import org.androidannotations.annotations.EActivity;
+import ru.megzlo.templateapp.R;
 
 /**
  * @author paradoxfm - 28.09.2015
  */
-
+@EActivity(R.layout.home)
 public class Home extends Activity {
 
     @Override
